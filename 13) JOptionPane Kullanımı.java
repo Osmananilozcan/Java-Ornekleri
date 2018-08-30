ebd5210324bec36b641780caa829fac0e80a4a1d
Osman Anıl Özcan <osmananilozcan@gmail.com>
2018-08-30 14:58:16 +0300

    JOptionPane Kullanımı

--- 13) JOptionPane Kullanımı.java
+++ 13) JOptionPane Kullanımı.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 
 public class JOptionPaneKullanimi {
     public static void main(String[] args) {
+    
         // Sadece String tipinde alınabiliyor.
         String sayi = JOptionPane.showInputDialog("Bir sayı giriniz");
     }
