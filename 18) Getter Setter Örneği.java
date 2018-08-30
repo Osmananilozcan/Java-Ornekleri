4f35de31b39ab10e338d469b3992c3adb46d10a0
Osman Anıl Özcan <osmananilozcan@gmail.com>
2018-08-30 17:26:23 +0300

    Ogrenci.java sınıfı

---
+++ 18) Getter Setter Örneği.java
@@ -0,0 +1,36 @@
+public class Ogrenci {
+    private String ad, soyad, bolum;
+    private long numara;
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public long getNumara() {
+        return numara;
+    }
+
+    public void setNumara(long numara) {
+        this.numara = numara;
+    }
+}
