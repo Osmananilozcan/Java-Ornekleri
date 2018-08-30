963f65083bfc47b17a60e4eac94fb87f41ad5c5f
Osman Anıl Özcan <osmananilozcan@gmail.com>
2018-08-30 14:54:36 +0300

    Girilen Sayının Rakamları Toplamı

--- 9) Girilen Sayının Rakamları Toplamı.java
+++ 9) Girilen Sayının Rakamları Toplamı.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 public class GirilenSayininRakamlariToplami {
     public static void main(String[] args) {
         int sonuc = 0;
+        
         // Scanner nesnesi oluşturuluyor.
         Scanner veriAl = new Scanner(System.in);
+        
         // Kullanıcıdan sayı alınıyor.
         int veri1 = veriAl.nextInt();
+        
         while (veri1 > 0) {
+        
             // Mod ile son rakamı sonuc değişkenine ekliyoruz.
             sonuc += (veri1 % 10);
+            
             /** 10'a bölerek son rakamı virgül sonrasına atıyoruz.
              * int değerlerde virgülden sonrasını almadığı için otomatikman
              * son rakam siliniyor ve tekrar modu alınarak aynı işlem
@@ -18,6 +23,7 @@ public class GirilenSayininRakamlariToplami {
              */
             veri1 = veri1 / 10;
         }
+        
         // sonuc değişkeni ekrana yazdırılıyor.
         System.out.println("Girdiğiniz sayının rakamları toplamı: " + sonuc);
     }
