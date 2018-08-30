7a12c5e151f1aaa82521ed86abc9394f027e797a
Osman Anıl Özcan <osmananilozcan@gmail.com>
2018-08-30 14:53:58 +0300

    Diziden Rastgele Eleman Seçme

--- 8) Diziden Rastgele Eleman Seçme.java
+++ 8) Diziden Rastgele Eleman Seçme.java
@@ -3,12 +3,16 @@ import java.util.Random;
 
 public class DizidenRastgeleElemanSecme {
     public static void main(String[] args) {
+    
         // Elle istediğimiz isimleri yazıyoruz.
         String[] isimlerDizi = {"Osman", "Anıl", "Zafer", "Musa", "Furkan", "Tahir", "Uğur", "Samet"};
+        
         // Rastgele bir sayı için nesne oluşturuyoruz.
         Random rastgele = new Random();
+        
         // isimlerDizi'nin uzunluğunda bir rastgele sayı üretmesini sağlıyoruz.
         int kacinciEleman = rastgele.nextInt(isimlerDizi.length);
+        
         // Üretilen sayıyı dizinin indis değerine verip yazdırıyoruz.
         System.out.println("Rastgele seçilen isim: " + isimlerDizi[kacinciEleman]);
     }
