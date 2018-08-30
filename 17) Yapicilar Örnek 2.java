72075f66fbdcbd4515ce15d56faaade9d5030f71
Osman Anıl Özcan <osmananilozcan@gmail.com>
2018-08-30 15:47:27 +0300

    YapicilarOverLoading.java sınıfı

---
+++ 17) Yapicilar Örnek 2.java
@@ -0,0 +1,7 @@
+public class YapicilarOverLoading {
+    public static void main(String[] args) {
+      Yapicilar nesne1 = new Yapicilar();
+      Yapicilar nesne2 = new Yapicilar("Osman Anıl", "Özcan");
+      Yapicilar nesne3 = new Yapicilar(2, 5);
+    }
+}
